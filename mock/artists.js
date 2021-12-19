393441177c0c086ef2228c55e479fd30538118f6
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-18 18:59:54 -0800

    initial commit

---
+++ mock/artists.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 0,
+    artist: 'The Jesus and Mary Chain',
+    origin: 'East Kilbride, Scotland',
+    years_active: '1983-1999, 2007-present',
+  },
+  {
+    id: 1,
+    artist: 'The Velvet Underground',
+    origin: 'New York City, New York, U.S.',
+    years_active: '1964-1973, 1990, 1992-1993, 1996',
+  },
+  {
+    id: 2,
+    artist: 'Melt-Banana',
+    origin: 'Tokyo, Japan',
+    years_active: '1992-present',
+  },
+  {
+    id: 3,
+    artist: 'Motörhead',
+    origin: '	London, England',
+    years_active: '1975-2015',
+  },
+];
