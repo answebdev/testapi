275ff9cebee069a29c7c1e9ad506886a7a22a487
ANS <36783010+answebdev@users.noreply.github.com>
2021-12-18 19:30:08 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./mock/db');
+const router = jsonServer.router('./mock/db.js');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 const albums = require('./mock/albums');
 const artists = require('./mock/artists');
+// module.exports = () => ({
+//   albums,
+//   artists,
+// });
+
+server.use(middlewares);
+server.use(router);
+
 module.exports = () => ({
   albums,
   artists,
 });
-
-server.use(middlewares);
-server.use(router);
 
 server.listen(port);
